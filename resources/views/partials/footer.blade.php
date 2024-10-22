992d017513d023ee5a354d2d618268baaa43ea30
natdm02 <natalidimarino2002@gmail.com>
2024-10-22 18:25:24 +0200

    [add] first commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+
+<footer>
+    <div class="footer-content">
+
+        <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+
+    <nav>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+
+        </ul>
+
+    </nav>
+
+</header>
